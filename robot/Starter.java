daa7392c0f7e0b1fe3d0a2bd2c32a276f2adbbba
pccoder <134186013+pedroecartagena@users.noreply.github.com>
2025-08-15 18:25:09 +0000

    Redid code for easier readability

--- robot/Starter.java
+++ robot/Starter.java
@@ -14,84 +14,16 @@ public class Starter implements Directions{
     // The line below creates a Robot that we will refer to as rob
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
-    Robot rob = new Robot(15,2,South,50);
+    Robot rob = new Robot(4,2,North,50);
     Robot bob = new Robot(15, 11,South,50);
 
-    // Want a second robot?  No prob.  They are cheap :)
-    //Robot dude = new Robot(7,5,West,9);
-    // examples of commands you can invoke on a Robot
-    rob.move();// move one step in the direction it is facing
-
-
     //letter P
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
-    rob.move();
-    rob.putBeeper();
+    for(int i = 0; i <= 10; i++) {
+      rob.move();
+      rob.putBeeper();
+    }
     rob.turnLeft();
     rob.turnLeft();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-    rob.move();
-
-    // letter C
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.move();
-    bob.putBeeper();
-    bob.turnLeft();
-    bob.turnLeft();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
-    bob.move();
+    rob.turnLeft();
   }
 }
