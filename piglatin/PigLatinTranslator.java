5cab881164d3cfb3152ffb9d9a956fe9253d718a
pccoder <134186013+pedroecartagena@users.noreply.github.com>
2025-10-21 21:09:15 +0000

    Tried to do some things to work but it stopped doing all the constonants. Gonna work on it later.

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -21,6 +21,7 @@ public class PigLatinTranslator {
         // The input to this function could be any English string.
         // It may be made up of many words.
         // This method must call translateWord once for each word in the string.
+
         result = translateWord(input);
 
         return result;
@@ -34,7 +35,18 @@ public class PigLatinTranslator {
         // TODO: Replace this code to correctly translate a single word.
         // Start here first!
         // This is the first place to work.
-        result = input; // delete this line
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if ("aeiou".indexOf(c) != -1) {
+                if (Character.isUpperCase(c)){
+                    result = input.substring(0, i) + input.substring(i) + input.substring(0, i).toLowerCase() + "ay";
+                }
+                else {
+                    result = input.substring(i) + input.substring(0, i).toLowerCase() + "ay";
+                }
+                break;
+            }
+        }
 
         return result;
     }
