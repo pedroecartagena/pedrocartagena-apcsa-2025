735b7f37ce05350fa5ee127018c5cc7901b54cd4
pccoder <134186013+pedroecartagena@users.noreply.github.com>
2026-01-07 23:06:48 +0000

    did the first project warm up

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
-
+            int sum = 0;
+            for (int i = 1; i <= 1000; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    sum += i;
+                }
+            }
+            System.out.println(sum);
     }
 }
