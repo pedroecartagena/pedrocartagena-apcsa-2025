ee71e052cf4842265b393cc482ea3960ea8b64a7
pccoder <134186013+pedroecartagena@users.noreply.github.com>
2026-04-01 21:22:08 +0000

    finished solve method, honestly not too bad just had to search google, also passed tests

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -11,14 +11,19 @@ public class TowerSolver {
     public void solve(TowerModel model)
     {
         this.model = model;
-        solver(model.height(), 0, 2, 1);
+        solve(model.height(), 0, 2, 1);
         // Call the missing solve method (not this one)
     }
 
     // Create an overloaded solve(...) method
     public void solve(int height, int source, int destination, int spare)
     {
-        
+        if (height <= 0){
+            return;
+        }
+        solve(height - 1, source, spare, destination);
+        model.move(source, destination);
+        solve(height - 1, spare, destination, source);  
     }
 
 }
